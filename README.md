8d08de1e670543c049c751bd30aa0d6f6a9716bf
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-05-08 23:56:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# navbar-dropdown-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ugbtsy)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="home">Home</h1>
+      <img src="https://images.pexels.com/photos/6991567/pexels-photo-6991567.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>jknmmnmbmb,bmn
+      <p>tyfyfgujh  bvjhjhkj nbkj</p>
+    </div>
+  );
+}
+
+export default Home;
